0b8ec6306e35aaabffd287ab463500f16a9df1a2
Luca <58145073+luca959@users.noreply.github.com>
2022-03-15 17:34:40 +0100

    Add files via upload

--- static/db.py
+++ static/db.py
@@ -4,3 +4,8 @@ with open('items.sql') as f:
     connection.executescript(f.read())
 connection.commit()
 connection.close()
+connection=sqlite3.connect('database2.db')
+with open('user.sql') as f:
+    connection.executescript(f.read())
+connection.commit()
+connection.close()
---
+++ static/user.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS user;
+CREATE TABLE user(
+    email TEXT NOT NULL,
+    pwd TEXT NOT NULL,
+    PRIMARY KEY (email)
+  
+);
